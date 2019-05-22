2bef6e99192fb917d24610853b248182b4e42fab
Frari <francescoriccardi86@gmail.com>
2019-05-22 15:31:22 +0200

    problema con append

--- main.js
+++ main.js
@@ -4,3 +4,24 @@
 // Inizialmente chiamate l'API con un numero fisso stabilito da voi,
 // in modo da interrogare l'API nel modo corretto e stilare le card.
 // Poi aggiungete la richiesta del numero dei giocatori all'utente
+$(document).ready(function(){
+  $.ajax({
+    url:'https://www.boolean.careers/api/array/basket?',
+    metod:'GET',
+    data:{
+      'n':5
+    },
+    success: function(data){
+      console.log(data);
+      // var schede = data.response;
+
+      for(i=0; i<data.response.length; i++){
+        $('.container').append(data.response);
+      };
+
+    },
+    error: function(){
+      alert('errore');
+    }
+  });
+});
