b3fa977815770f748fbf413dd16de1c4f7440656
Frari <francescoriccardi86@gmail.com>
2019-05-22 18:01:53 +0200

    fine

--- main.js
+++ main.js
@@ -6,36 +6,38 @@
 // Poi aggiungete la richiesta del numero dei giocatori all'utente
 $(document).ready(function(){
 
+// chiedo con un prompt all'utente quante schede giocatore vuole vedere
+  var utente_num=parseInt(prompt('iserisci qui il numero delle schede che uoi vedere'));
+  console.log(utente_num);
+
 // effettuo chiamata ajax per richiedere array oggetti giocatori
   $.ajax({
     url:'https://www.boolean.careers/api/array/basket?',
     metod:'GET',
     data:{
-      'n':5
+      'n':utente_num
     },
     success: function(data){
       console.log(data);
 
 // creo ciclo per stampare a schermo tutte le proprietà dei giocatori
-      for(var i=0; i<data.response.length; i++){
-        $('.container').append(data.response[i].playerCode);
-        $('.container').append(data.response[i].rebounds);
-        $('.container').append(data.response[i].fouls);
-        $('.container').append(data.response[i].twoPoints);
-        $('.container').append(data.response[i].threePoints);
-
-      };
 // creo un template per visualizzare le schede dei giocatori
+
       for(var i=0; i<data.response.length; i++){
-        var template_giocatore=$('.template_player').html();
+
+        var template_giocatore=$('#template_player').html();
         var template_function = Handlebars.compile(template_giocatore);
-        var context{
-
+        var context={
+          codice:data.response[i].playerCode,
+          rimbalzi:data.response[i].rebounds,
+          falli:data.response[i].fouls,
+          punti2:data.response[i].twoPoints,
+          punti3:data.response[i].threePoints
         }
-        var html= template(context);
+        var html= template_function(context);
         $('.container').append(html);
-
       };
+
 
     },
     error: function(){
